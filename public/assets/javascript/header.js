5e69a2045d23472efde9e611d28d8d7a5daa6cd1
Prism3 <ben.ayoube@outlook.com>
2022-05-18 00:01:36 +0100

    Header Update

--- public/assets/javascript/header.js
+++ public/assets/javascript/header.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const hamburger = document.querySelector('.haydora .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.haydora .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.haydora .nav-bar .nav-list ul li a');
+const haydora = document.querySelector('.haydora.header-container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -11,9 +11,9 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		haydora.style.backgroundColor = '#29323c';
 	} else {
-		header.style.backgroundColor = 'transparent';
+		haydora.style.backgroundColor = 'transparent';
 	}
 });
 
